f26b9e0561caea8dd74fd71169515669d7bcc212
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-08-13 09:20:32 +0530

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import navlogo from '../../assets/logo.png'
 import navProfile from '../../assets/nav-profile.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <div className='admin'>
         <img src={navlogo} alt="" className="nav_logo" />
+        <h3>GROCEREASE <span >ADMIN PANNEL</span> </h3>
+        </div>
         <img src={navProfile} className='nav-profile' alt="" />
     </div>
   )
